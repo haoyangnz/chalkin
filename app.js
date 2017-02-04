205a2b4a3b3e6b678e04b07dc3230ccc28656d91
Haoyang Feng <haoyangnz@gmail.com>
2017-02-04 14:10:35 +1300

    Serve index.html

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile('index.html', { root : __dirname})
 })
 
 app.listen(3000, function () {
