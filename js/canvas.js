e968978d443f6d1c46322932f40aa8a7e276c926
Haoyang Feng <haoyangnz@gmail.com>
2017-02-04 18:20:17 +1300

    Scrooling horizontally

--- js/canvas.js
+++ js/canvas.js
@@ -6,15 +6,24 @@ textGuide.strokeWidth = 3
 var start
 var end
 
+var x = 0
+var y = 0
 
-get('item?x=0&y=0&width=' + view.size.width + '&height=' + view.size.height, function(res) {
-	json = JSON.parse(res)
-	console.log(json)
-	console.log(JSON.parse(json[0].item))
-	json.forEach(function(onejson) {
-		project.activeLayer.importJSON(JSON.parse(onejson.item))
+load()
+
+function load() {
+	project.activeLayer.removeChildren()
+	get('item?x=' + x + '&y=' + y + '&width=' + view.size.width + '&height=' + view.size.height, function(res) {
+		json = JSON.parse(res)
+		console.log(json)
+		json.forEach(function(onejson) {
+			var item = JSON.parse(onejson.item)
+			item[1].matrix[4] -= x
+			item[1].matrix[5] -= y
+			project.activeLayer.importJSON(item)
+		})
 	})
-})
+}
 
 function onMouseDown(event) {
 	start = event.point
@@ -40,11 +49,28 @@ function onMouseUp(event) {
   var xhr = new XMLHttpRequest()
   xhr.open('POST', 'item', true)
   xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
-  xhr.send(JSON.stringify({ bounds: {x: text.bounds.x, y: text.bounds.y, width: text.bounds.width, height: text.bounds.height}, item: text.exportJSON() }))
+	var item = JSON.parse(text.exportJSON())
+	item[1].matrix[4] += x
+	item[1].matrix[5] += y
+	project.activeLayer.importJSON(item)
+  xhr.send(JSON.stringify({ bounds: {x: text.bounds.x, y: text.bounds.y, width: text.bounds.width, height: text.bounds.height}, item: JSON.stringify(item)  }))
   xhr.onloadend = function () {
 		// done
   }
 
+}
+
+function onKeyDown(event) {
+	if (event.key == 'left') {
+		x -= view.size.width / 4;
+		project.activeLayer.position = [x,y]
+		load()
+	}
+	if (event.key == 'right') {
+		x += view.size.width / 4;
+		project.activeLayer.position = [x,y]
+		load()
+	}
 }
 
 function get(url, callback)
