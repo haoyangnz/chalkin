aa2bcce8f7cee6390150dc64ca831289a8548401
Haoyang Feng <haoyangnz@gmail.com>
2017-02-06 17:16:02 +1300

    Reorganise code and introduce modes

--- js/canvas.js
+++ js/canvas.js
@@ -1,3 +1,5 @@
+var mode = 'move'
+
 var chalk = '#BCBDB8'
 
 var start
@@ -7,57 +9,47 @@ var textGuide
 var x = 0
 var y = 0
 
+mode = 'text'
 load()
 
-function load() {
-	project.activeLayer.removeChildren()
-	textGuide = new Path()
-	textGuide.strokeColor = chalk
-	textGuide.strokeWidth = 3
-	get('item?x=' + x + '&y=' + y + '&width=' + view.size.width + '&height=' + view.size.height, function(res) {
-		json = JSON.parse(res)
-		json.forEach(function(onejson) {
-			var item = JSON.parse(onejson.item)
-			item[1].matrix[4] -= x
-			item[1].matrix[5] -= y
-			project.activeLayer.importJSON(item)
-		})
-	})
-}
-
 function onMouseDown(event) {
-	start = event.point
+	if (mode == 'text') {
+		start = event.point
+	}
 }
 
 function onMouseDrag(event) {
-	textGuide.removeSegments()
-	textGuide.add(start, event.point)
+	if (mode == 'text') {
+		textGuide.removeSegments()
+		textGuide.add(start, event.point)
+	}
 }
 
 function onMouseUp(event) {
-	end = event.point
-	textGuide.removeSegments()
-	var input = prompt('Input text', '')
-	var text = new PointText(start)
-	text.content = input
-	text.fillColor = chalk
-	text.fontSize = 30
-	text.fontFamily = 'Comic Sans MS'
-	text.rotate(Math.atan((end.y - start.y) / (end. x - start.x)) * 180 / (Math.PI), start)
-	
-  // Store object
-  var xhr = new XMLHttpRequest()
-  xhr.open('POST', 'item', true)
-  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
-	var item = JSON.parse(text.exportJSON())
-	item[1].matrix[4] += x
-	item[1].matrix[5] += y
-	project.activeLayer.importJSON(item)
-  xhr.send(JSON.stringify({ bounds: {x: text.bounds.x + x, y: text.bounds.y + y, width: text.bounds.width, height: text.bounds.height}, item: JSON.stringify(item)  }))
-  xhr.onloadend = function () {
-		// done
-  }
-
+	if (mode == 'text') {
+		end = event.point
+		textGuide.removeSegments()
+		var input = prompt('Input text', '')
+		var text = new PointText(start)
+		text.content = input
+		text.fillColor = chalk
+		text.fontSize = 30
+		text.fontFamily = 'Comic Sans MS'
+		text.rotate(Math.atan((end.y - start.y) / (end. x - start.x)) * 180 / (Math.PI), start)
+		
+		// Store object
+		var xhr = new XMLHttpRequest()
+		xhr.open('POST', 'item', true)
+		xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
+		var item = JSON.parse(text.exportJSON())
+		item[1].matrix[4] += x
+		item[1].matrix[5] += y
+		project.activeLayer.importJSON(item)
+		xhr.send(JSON.stringify({ bounds: {x: text.bounds.x + x, y: text.bounds.y + y, width: text.bounds.width, height: text.bounds.height}, item: JSON.stringify(item)  }))
+		xhr.onloadend = function () {
+			// done
+		}
+	}
 }
 
 function onKeyDown(event) {
@@ -83,8 +75,23 @@ function onKeyDown(event) {
 	}
 }
 
-function get(url, callback)
-{
+function load() {
+	project.activeLayer.removeChildren()
+	textGuide = new Path()
+	textGuide.strokeColor = chalk
+	textGuide.strokeWidth = 3
+	get('item?x=' + x + '&y=' + y + '&width=' + view.size.width + '&height=' + view.size.height, function(res) {
+		json = JSON.parse(res)
+		json.forEach(function(onejson) {
+			var item = JSON.parse(onejson.item)
+			item[1].matrix[4] -= x
+			item[1].matrix[5] -= y
+			project.activeLayer.importJSON(item)
+		})
+	})
+}
+
+function get(url, callback) {
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.onreadystatechange = function() { 
 		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp.response);
